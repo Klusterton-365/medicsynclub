ff7719a16d60260a2bd6579cad5017e85eaf2ce9
Dev. Alex_dgreat <105602085+devalexdgreat@users.noreply.github.com>
2023-11-25 00:11:18 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-
 window.addEventListener('load', function() {
 	const loader = document.getElementById('loader').style.display = "none";
-}) 
+}); 
+
+let menu = document.getElementById("menu");
+let isOpened = false;
+function menuController() {
+	if(isOpened === false) {
+		menu.classList.remove("hidden");
+		isOpened = true;
+	} else {
+		menu.classList.add("hidden");
+		isOpened = false;
+	}
+}
