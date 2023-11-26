90e53d6ac262acf27aa5b79b2dfbcc97cf28f568
Kamsi <ckamsi04@gmail.com>
2023-11-27 00:49:40 +0100

    finished signup and signin implementation with ajax

--- app.js
+++ app.js
@@ -13,3 +13,23 @@ function menuController() {
 		isOpened = false;
 	}
 }
+
+function logout() {
+	toastr.info('Are you sure you want to logout?', 'Confirmation', {
+	  closeButton: true,
+	  timeOut: 0,
+	  extendedTimeOut: 0,
+	  tapToDismiss: false,
+	  preventDuplicates : true,
+	  onclick: function (e) {
+		if (e.target.classList.contains('toastr-close-button')) {
+		  toastr.clear();
+		} else {
+		  // User clicked outside the notification, perform logout actions
+		  sessionStorage.removeItem('user');
+		  localStorage.removeItem('token');
+		  window.location = "dashboard";
+		}
+	  }
+	});
+}
